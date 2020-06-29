636562f5d1041f78cd3392e5232e011625262356
Jimin Byun <jiminbyun@google.com>
2020-06-29 04:45:19 +0000

    Get user location, center the map, display marker

--- web-app/src/main/webapp/script.js
+++ web-app/src/main/webapp/script.js
@@ -12,9 +12,45 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+var map, infoWindow;
 function initMap() {
-  const map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 37.422, lng: -122.084 },
-    zoom: 5,
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 6,
   });
+  infoWindow = new google.maps.InfoWindow();
+
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      function (position) {
+        var userPosition = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        };
+
+        const marker = new google.maps.Marker({
+          position: userPosition,
+          map: map,
+        });
+        map.setCenter(userPosition);
+      },
+      function () {
+        handleLocationError(true, infoWindow, map.getCenter());
+      }
+    );
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(
+    browserHasGeolocation
+      ? "Error: The Geolocation service failed."
+      : "Error: Your browser doesn't support geolocation."
+  );
+  infoWindow.open(map);
 }
