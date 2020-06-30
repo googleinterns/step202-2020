841bbae7587ec67f79104a2646556b109129174b
Jimin Byun <jiminb159@gmail.com>
2020-06-30 23:10:52 +0000

    Add login servlet that sends login/logout url

---
+++ web-app/src/main/java/com/google/sps/servlets/LoginServlet.java
@@ -0,0 +1,26 @@
+package com.google.sps.servlets;
+
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    UserService userService = UserServiceFactory.getUserService();
+    response.setContentType("text/plain");
+    String url;
+    if (!userService.isUserLoggedIn()) {
+      url = userService.createLoginURL("/index.html")
+    } else {
+      url = userService.createLogoutURL("/index.html")
+    }
+    response.getWriter().println(url);
+  }
+}
