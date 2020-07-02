a2d69782f86a0c5089ccc205c1c442c85a0a9344
Jimin Byun <jiminb159@gmail.com>
2020-07-02 01:04:52 +0000

    Add python script to convert from csv to json

---
+++ web-app/preprocess/csvToJson.py
@@ -0,0 +1,19 @@
+import csv
+import json
+
+csvFilePath = '2020_04_london.csv'
+jsonFilePath = '2020_04_london.json'
+
+data = []
+with open(csvFilePath) as csvFile:
+    csvReader = csv.DictReader(csvFile)
+    for rows in csvReader:
+        entry = {}
+        entry['Month'] = rows['Month']
+        entry['Longitude'] = rows['Longitude']
+        entry['Latitude'] = rows['Latitude']
+        entry['Crime type'] = rows['Crime type']
+        data.append(entry)
+
+with open(jsonFilePath, 'w') as jsonFile:
+    jsonFile.write(json.dumps(data, indent=2))
