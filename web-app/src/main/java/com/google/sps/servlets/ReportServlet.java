c66b2c99b5f6d3fda14399c057e14c0d78e48d6a
Ashley Wong <ashleywong@college.harvard.edu>
2020-07-02 04:18:15 +0000

    tidy code

--- web-app/src/main/java/com/google/sps/servlets/ReportServlet.java
+++ web-app/src/main/java/com/google/sps/servlets/ReportServlet.java
@@ -115,45 +115,9 @@ public class ReportServlet extends HttpServlet {
     // User submitted form without selecting a file, so we can't get a URL. (dev
     // server)
     if (blobKeys == null || blobKeys.isEmpty()) {
-      System.out.println("Empty blobkey\n\n");
       return null;
     } else {
-      System.out.println("Present blobkey\n\n");
-      System.out.println(blobKeys.get(0).getKeyString());
-      String url = "/serve?blob-key=" + blobKeys.get(0).getKeyString();
-      return url;
+      return "/serve?blob-key=" + blobKeys.get(0).getKeyString();
     }
-
-    // Our form only contains a single file input, so get the first index.
-    // BlobKey blobKey = blobKeys.get(0);
-
-    // // User submitted form without selecting a file, so we can't get a URL. (live
-    // // server)
-    // BlobInfo blobInfo = new BlobInfoFactory().loadBlobInfo(blobKey);
-    // if (blobInfo.getSize() == 0) {
-    // blobstoreService.delete(blobKey);
-    // return null;
-    // }
-
-    // // We could check the validity of the file here, e.g. to make sure it's an
-    // image
-    // // file
-    // // https://stackoverflow.com/q/10779564/873165
-
-    // // Use ImagesService to get a URL that points to the uploaded file.
-    // ImagesService imagesService = ImagesServiceFactory.getImagesService();
-    // ServingUrlOptions options = ServingUrlOptions.Builder.withBlobKey(blobKey);
-
-    // // To support running in Google Cloud Shell with AppEngine's devserver, we
-    // must
-    // // use the relative
-    // // path to the image, rather than the path returned by imagesService which
-    // // contains a host.
-    // try {
-    // URL url = new URL(imagesService.getServingUrl(options));
-    // return url.getPath();
-    // } catch (MalformedURLException e) {
-    // return imagesService.getServingUrl(options);
-    // }
   }
 }
