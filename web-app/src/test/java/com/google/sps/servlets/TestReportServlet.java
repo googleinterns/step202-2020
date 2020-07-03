d4b2b479ce1348351096965ac63341ede1715658
Jimin Byun <jiminb159@gmail.com>
2020-07-03 05:49:01 +0000

    Edit class to the testing file

---
+++ web-app/src/test/java/com/google/sps/servlets/TestReportServlet.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class TestReportServlet {
+  @Test
+  public void testAddition() {
+    assertEquals(3, 2 + 2);
+  }
+}
