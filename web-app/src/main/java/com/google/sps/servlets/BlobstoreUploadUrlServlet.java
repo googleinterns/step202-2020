d91f0b94106eebdc39b7e4f41e3eb17cd424b838
Ashley Wong <ashleywong@college.harvard.edu>
2020-07-03 00:48:52 +0000

    remove print statements

--- web-app/src/main/java/com/google/sps/servlets/BlobstoreUploadUrlServlet.java
+++ web-app/src/main/java/com/google/sps/servlets/BlobstoreUploadUrlServlet.java
@@ -23,21 +23,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * When the fetch() function requests the /blobstore-upload-url URL, the content
- * of the response is the URL that allows a user to upload a file to Blobstore.
- * If this sounds confusing, try running a dev server and navigating to
- * /blobstore-upload-url to see the Blobstore URL.
+ * When the fetch() function requests the /blobstore-upload-url URL, the content of the response is
+ * the URL that allows a user to upload a file to Blobstore. If this sounds confusing, try running a
+ * dev server and navigating to /blobstore-upload-url to see the Blobstore URL.
  */
 @WebServlet("/blobstore-upload-url")
 public class BlobstoreUploadUrlServlet extends HttpServlet {
 
-    @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        System.out.println("java blobstore url accessed");
-        BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
-        String uploadUrl = blobstoreService.createUploadUrl("/report");
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
+    String uploadUrl = blobstoreService.createUploadUrl("/report");
 
-        response.setContentType("text/html");
-        response.getWriter().println(uploadUrl);
-    }
+    response.setContentType("text/html");
+    response.getWriter().println(uploadUrl);
+  }
 }
