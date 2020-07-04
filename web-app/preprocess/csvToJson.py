2f9094dcb438a58becd895f5788b6bbcffd39a93
Jimin Byun <jiminb159@gmail.com>
2020-07-04 14:44:54 +0000

    Use __main__ for executable scripts

--- web-app/preprocess/csvToJson.py
+++ web-app/preprocess/csvToJson.py
@@ -3,20 +3,25 @@
 import csv
 import json
 
-CSV_FILE_PATH = '2020_05_london.csv'
-JSON_FILE_PATH = '2020_05_london.json'
+def csvToJson(): 
+  CSV_FILE_PATH = '2020_05_london.csv'
+  JSON_FILE_PATH = '2020_05_london.json'
 
-data = []
-with open(CSV_FILE_PATH) as csvFile:
-    csvReader = csv.DictReader(csvFile)
-    for rows in csvReader:
-        entry = {
-          'month': rows['Month'],
-          'longitude': rows['Longitude'],
-          'latitude': rows['Latitude'],
-          'crimeType': rows['Crime type']
-        }
-        data.append(entry)
+  data = []
+  with open(CSV_FILE_PATH) as csvFile:
+      csvReader = csv.DictReader(csvFile)
+      for rows in csvReader:
+          entry = {
+            'month': rows['Month'],
+            'longitude': rows['Longitude'],
+            'latitude': rows['Latitude'],
+            'crimeType': rows['Crime type']
+          }
+          data.append(entry)
 
-with open(JSON_FILE_PATH, 'w') as jsonFile:
-    jsonFile.write(json.dumps(data, indent=2))
+  with open(JSON_FILE_PATH, 'w') as jsonFile:
+      jsonFile.write(json.dumps(data, indent=2))
+  
+if __name__ == "__main__":
+  csvToJson()
+
