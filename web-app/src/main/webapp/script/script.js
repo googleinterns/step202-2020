a8c289d6a77c4f6ca7b12f1850a4d7497e3266d6
Ashley Wong <ashleywong@college.harvard.edu>
2020-07-15 00:03:04 +0000

    fix formatting

--- web-app/src/main/webapp/script/script.js
+++ web-app/src/main/webapp/script/script.js
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-import { setDirections } from 'script/directions.js'
+import { setDirections } from "script/directions.js";
 
 let mapMarkers = [];
 
@@ -22,33 +22,47 @@ window.onload = async () => {
   const map = initMap();
   // Search bar
   directionsRenderer.setMap(map);
-  document.getElementById('search-location').addEventListener('keydown', (e) => {
+  document.getElementById("search-location").addEventListener("keydown", (e) => {
     if (e.code === "Enter") {
       e.preventDefault();
-      setDirections(directionsService, directionsRenderer, getUserLocation(), document.getElementById('search-location').value);
+      setDirections(
+        directionsService,
+        directionsRenderer,
+        getUserLocation(),
+        document.getElementById("search-location").value
+      );
     }
   });
-  document.getElementById('report-button').addEventListener('click', () => showReportForm(map, geocoder));
-  document.getElementById('back-icon').addEventListener('click', () => {
+  document
+    .getElementById("report-button")
+    .addEventListener("click", () => showReportForm(map, geocoder));
+  document.getElementById("back-icon").addEventListener("click", () => {
     hideReportForm();
-    document.getElementById('report-form').reset();
+    document.getElementById("report-form").reset();
   });
-  document.getElementById('map-icon').addEventListener('click', () => hideReportForm)
-  document.getElementById('submit-button').addEventListener('click', () => postUserReport(geocoder));
-  document.getElementById('menu-button').addEventListener('click',
-    () => { document.getElementById('menu').style.display = 'block' });
-  document.getElementById('close-menu').addEventListener('click',
-    () => document.getElementById('menu').style.display = 'none');
+  document.getElementById("map-icon").addEventListener("click", () => hideReportForm);
+  document
+    .getElementById("submit-button")
+    .addEventListener("click", () => postUserReport(geocoder));
+  document.getElementById("menu-button").addEventListener("click", () => {
+    document.getElementById("menu").style.display = "block";
+  });
+  document
+    .getElementById("close-menu")
+    .addEventListener("click", () => (document.getElementById("menu").style.display = "none"));
   fetchMarkers(map);
   const timeFrameOptions = document.getElementById("time-frame-options");
-  timeFrameOptions.addEventListener('change', () => { loadPoliceReports(map) });
+  timeFrameOptions.addEventListener("change", () => {
+    loadPoliceReports(map);
+  });
   const categoryOptions = document.getElementById("category-options");
-  categoryOptions.addEventListener('change', () => { loadPoliceReports(map) });
+  categoryOptions.addEventListener("change", () => {
+    loadPoliceReports(map);
+  });
   loadPoliceReports(map);
   displayUserLocation(map);
   await setLoginStatus();
 };
-
 
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
@@ -59,7 +73,7 @@ function initMap() {
 }
 
 async function fetchMarkers(map) {
-  const response = await fetch('/report');
+  const response = await fetch("/report");
   const markers = await response.json();
   let uiState = { activeInfoWindow: null };
 
@@ -67,20 +81,22 @@ async function fetchMarkers(map) {
     createMarkerForDisplay(map, marker, uiState);
   });
 
-  map.addListener('click', () => {
+  map.addListener("click", () => {
     if (uiState.activeInfoWindow) {
       uiState.activeInfoWindow.close();
       uiState.activeInfoWindow = null;
     }
-  })
+  });
 }
 
 function createMarkerForDisplay(map, data, uiState) {
-  const marker =
-    new google.maps.Marker({ position: { lat: data.latitude, lng: data.longitude }, map: map });
+  const marker = new google.maps.Marker({
+    position: { lat: data.latitude, lng: data.longitude },
+    map: map,
+  });
 
   const infoParagraph = document.createElement("div");
-  infoParagraph.setAttribute('id', 'info-window');
+  infoParagraph.setAttribute("id", "info-window");
   const timestamp = new Date(data.timestamp);
   infoParagraph.innerHTML = `
     <h1>${data.title}</h1>
@@ -89,20 +105,21 @@ function createMarkerForDisplay(map, data, uiState) {
   `;
 
   if (data.imageUrl) {
-    infoParagraph.insertAdjacentHTML('beforeend',
+    infoParagraph.insertAdjacentHTML(
+      "beforeend",
       `<img src="${window.location.href}serve?blob-key=${data.imageUrl}"
-      id="info-image" alt="User-submitted image of incident">`)
+      id="info-image" alt="User-submitted image of incident">`
+    );
   }
 
   const infoWindow = new google.maps.InfoWindow({ content: infoParagraph });
-  marker.addListener('click', () => {
+  marker.addListener("click", () => {
     if (uiState.activeInfoWindow) {
       uiState.activeInfoWindow.close();
     }
     infoWindow.open(map, marker);
     uiState.activeInfoWindow = infoWindow;
   });
-
 }
 
 function getUserLocation() {
@@ -118,7 +135,7 @@ function getUserLocation() {
     () => {
       return null;
     }
-  )
+  );
 }
 
 function displayUserLocation(map) {
@@ -127,15 +144,16 @@ function displayUserLocation(map) {
   if (!navigator.geolocation) {
     showMessageOnInfoWindow(
       "Error: Your browser doesn't support geolocation.",
-      map.getCenter(), map, infoWindow);
+      map.getCenter(),
+      map,
+      infoWindow
+    );
     return;
   }
 
   const userPosition = getUserLocation();
   if (userPosition === null) {
-    showMessageOnInfoWindow(
-      "Please enable location services.",
-      map.getCenter(), map, infoWindow);
+    showMessageOnInfoWindow("Please enable location services.", map.getCenter(), map, infoWindow);
     return;
   }
 
@@ -153,57 +171,57 @@ function showMessageOnInfoWindow(message, position, map, infoWindow) {
 }
 
 function showReportForm(map, geocoder) {
-  document.getElementById('form-container').style.display = 'block';
-  const homeElements = document.getElementsByClassName('home');
+  document.getElementById("form-container").style.display = "block";
+  const homeElements = document.getElementsByClassName("home");
   for (const element of homeElements) {
-    element.style.display = 'none';
+    element.style.display = "none";
   }
 
-  geocoder.geocode({ 'location': map.getCenter() }, (results, status) => {
-    if (status === 'OK') {
+  geocoder.geocode({ location: map.getCenter() }, (results, status) => {
+    if (status === "OK") {
       if (results[0]) {
-        document.getElementById('location-input').value = results[0].formatted_address;
+        document.getElementById("location-input").value = results[0].formatted_address;
       } else {
-        console.error('No results found');
+        console.error("No results found");
       }
     } else {
-      console.error('Geocoder failed due to: ' + status);
+      console.error("Geocoder failed due to: " + status);
     }
-  })
+  });
 }
 
 function hideReportForm() {
-  document.getElementById('form-container').style.display = 'none';
+  document.getElementById("form-container").style.display = "none";
 
-  const homeElements = document.getElementsByClassName('home');
+  const homeElements = document.getElementsByClassName("home");
   for (const element of homeElements) {
-    element.style.display = 'block';
+    element.style.display = "block";
   }
 }
 
 // This currently gets the address from the report form's location field (no autopopulate, no map picker)
 async function postUserReport(geocoder) {
-  document.getElementById('report-form').reset();
+  document.getElementById("report-form").reset();
 
-  const address = document.getElementById('location-input').value;
-  geocoder.geocode({ 'address': address }, async (results, status) => {
-    if (status === 'OK') {
+  const address = document.getElementById("location-input").value;
+  geocoder.geocode({ address: address }, async (results, status) => {
+    if (status === "OK") {
       const coordinates = results[0].geometry.location;
       const data = reportFormToURLQuery(coordinates.lat(), coordinates.lng());
       const url = await fetchBlobstoreUrl();
-      fetch(url, { method: 'POST', body: data });
+      fetch(url, { method: "POST", body: data });
     } else {
-      console.error('Geocode was not successful: ' + status);
+      console.error("Geocode was not successful: " + status);
     }
-  })
+  });
 }
 
 function reportFormToURLQuery(latitude, longitude) {
   const PARAMS_FORM_MAP = new Map([
-    ['title-input', 'title'],
-    ['time-input', 'timestamp'],
-    ['category-input', 'incidentType'],
-    ['description-input', 'description'],
+    ["title-input", "title"],
+    ["time-input", "timestamp"],
+    ["category-input", "incidentType"],
+    ["description-input", "description"],
   ]);
 
   const formData = new FormData();
@@ -212,15 +230,15 @@ function reportFormToURLQuery(latitude, longitude) {
     formData.append(paramName, value);
   }
 
-  formData.append('latitude', latitude);
-  formData.append('longitude', longitude);
-  formData.append('image', document.getElementById('attach-image').files[0]);
+  formData.append("latitude", latitude);
+  formData.append("longitude", longitude);
+  formData.append("image", document.getElementById("attach-image").files[0]);
 
   return formData;
 }
 
 async function fetchBlobstoreUrl() {
-  const response = await fetch('/blobstore-upload-url');
+  const response = await fetch("/blobstore-upload-url");
   const imageURL = await response.text();
   return imageURL;
 }
@@ -232,18 +250,30 @@ async function loadPoliceReports(map) {
   }
   mapMarkers = [];
 
-  const FILE_NAMES = ['2019_12_london', '2020_01_london', '2020_02_london', '2020_03_london', '2020_04_london', '2020_05_london']
-  const uncheckedCategoriesElement = Array.from(document.querySelectorAll("input.category:not(:checked)"));
-  const uncheckedCategories = uncheckedCategoriesElement.map(element => element.value);
+  const FILE_NAMES = [
+    "2019_12_london",
+    "2020_01_london",
+    "2020_02_london",
+    "2020_03_london",
+    "2020_04_london",
+    "2020_05_london",
+  ];
+  const uncheckedCategoriesElement = Array.from(
+    document.querySelectorAll("input.category:not(:checked)")
+  );
+  const uncheckedCategories = uncheckedCategoriesElement.map((element) => element.value);
   const numberOfMonths = Number(document.querySelector("input.time-frame:checked").value);
 
-  const markersArrayForEachReports = await Promise.all(FILE_NAMES.map((file_name) =>
-    createPoliceReportMarkers(map, file_name, uncheckedCategories, numberOfMonths)));
+  const markersArrayForEachReports = await Promise.all(
+    FILE_NAMES.map((file_name) =>
+      createPoliceReportMarkers(map, file_name, uncheckedCategories, numberOfMonths)
+    )
+  );
   mapMarkers = markersArrayForEachReports.flat();
 }
 
 async function createPoliceReportMarkers(map, file_name, uncheckedCategories, numberOfMonths) {
-  const data = await fetch('../data/' + file_name + '.json');
+  const data = await fetch("../data/" + file_name + ".json");
   const reports = await data.json();
 
   const reportsDate = new Date();
@@ -262,12 +292,16 @@ async function createPoliceReportMarkers(map, file_name, uncheckedCategories, nu
     return displayCrimeType(uncheckedCategories, report.crimeType);
   });
 
-  const markers = filteredReports.map((report) => new google.maps.Marker({
-    position: {
-      lat: Number(report.latitude),
-      lng: Number(report.longitude)
-    }, map: map
-  }));
+  const markers = filteredReports.map(
+    (report) =>
+      new google.maps.Marker({
+        position: {
+          lat: Number(report.latitude),
+          lng: Number(report.longitude),
+        },
+        map: map,
+      })
+  );
 
   return markers;
 }
@@ -284,20 +318,26 @@ function displayCrimeType(uncheckedCategories, crimeType) {
 
 function isReportwithinTimeFrame(reportsDate, numberOfMonths) {
   const today = new Date();
-  const monthDiff = (today.getFullYear() - reportsDate.getFullYear()) * 12 + today.getMonth() + 1 - reportsDate.getMonth();
+  const monthDiff =
+    (today.getFullYear() - reportsDate.getFullYear()) * 12 +
+    today.getMonth() +
+    1 -
+    reportsDate.getMonth();
 
   return monthDiff < numberOfMonths;
 }
 
 async function setLoginStatus() {
-  const response = await fetch('/login');
+  const response = await fetch("/login");
   const loginStatus = await response.json();
 
-  const loginLogout = document.getElementById('login-logout');
+  const loginLogout = document.getElementById("login-logout");
   if (loginStatus.loggedIn) {
     loginLogout.innerText = "Logout";
   } else {
     loginLogout.innerText = "Login";
   }
-  loginLogout.addEventListener('click', () => { location.replace(loginStatus.url) });
+  loginLogout.addEventListener("click", () => {
+    location.replace(loginStatus.url);
+  });
 }
