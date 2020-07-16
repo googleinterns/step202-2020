5721ff75bb11d50cafa072261c250377e7674f32
Ashley Wong <ashleywong@college.harvard.edu>
2020-07-16 10:56:17 +0000

    remove dummy function

--- web-app/src/main/webapp/script/loadData.js
+++ web-app/src/main/webapp/script/loadData.js
@@ -6,10 +6,6 @@ class MapComponents {
 
 let mapComponents = new MapComponents();
 
-export function testFunc() {
-  return true;
-}
-
 export async function fetchAndParseJson(url) {
   const response = await fetch(url);
   const json = await response.json();
