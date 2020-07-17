5c6339cb0856ae4f921a43b0b2426a420bbaf3b9
Ashley Wong <ashleywong@college.harvard.edu>
2020-07-17 02:54:01 +0000

    print lat

--- web-app/src/main/webapp/script/directions.js
+++ web-app/src/main/webapp/script/directions.js
@@ -1,13 +1,17 @@
 export function setDirections(directionsService, directionsRenderer, origin, destination) {
   const request = {
     // Currently hardcoded because I can't enable geolocation on CRD
-    origin: { lat: -33.865143, lng: 151.2099 },
-    destination: destination,
-    travelMode: "DRIVING",
+    origin: { lat: 51.5196, lng: -0.1025 },
+    destination: { lat: 51.5141, lng: -0.0876 },
+    travelMode: "WALKING",
   };
+
   directionsService.route(request, (result, status) => {
     if (status == "OK") {
       directionsRenderer.setDirections(result);
+      for (let i = 1; i < 20; i++) {
+        console.log(result.routes[0].overview_path[i].toJSON().lat);
+      }
     } else if (status == "NOT_FOUND") {
       console.log("location could not be geocoded.");
     }
