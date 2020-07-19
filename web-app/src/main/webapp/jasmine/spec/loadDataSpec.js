b75c5888cb5c98fe9b67145670997abf1ec2ae87
Jimin Byun <jiminbyun@google.com>
2020-07-19 10:33:09 +0000

    Filter out old data

--- web-app/src/main/webapp/jasmine/spec/loadDataSpec.js
+++ web-app/src/main/webapp/jasmine/spec/loadDataSpec.js
@@ -1,17 +1,29 @@
 import { filterReports } from "../../script/loadData.js";
 
-describe("Filtering timeframe ", () => {
-  const sampleData = [
+describe("Filtering ", () => {
+  const sampleDataOld = [
     {
       yearMonth: "2019-12",
       longitude: "-0.111497",
       latitude: "51.518226",
       crimeType: "Other theft",
     },
+    {
+      yearMonth: "2019-12",
+      longitude: "-0.111497",
+      latitude: "51.518226",
+      crimeType: "Theft from the person",
+    },
+    {
+      yearMonth: "2019-12",
+      longitude: "-0.097562",
+      latitude: "51.518864",
+      crimeType: "Anti-social behaviour",
+    },
   ];
 
-  const filteredReports = filterReports(sampleData, [], 3);
-  it("should return true if within timeframe", () => {
-    expect(filteredReports.length).toEqual(0);
+  const filteredOldReports = filterReports(sampleDataOld, [], 3);
+  it("should correctly filter out too old data", () => {
+    expect(filteredOldReports.length).toEqual(0);
   });
 });
