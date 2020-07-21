868b81e9cb729043590bc69a4f9aac25f6d1019c
Ashley Wong <ashleywong@college.harvard.edu>
2020-07-21 00:20:11 +0000

    create tree

--- web-app/src/main/java/com/google/sps/servlets/AnalyticsServlet.java
+++ web-app/src/main/java/com/google/sps/servlets/AnalyticsServlet.java
@@ -1,21 +1,42 @@
+package com.google.sps.servlets;
+
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
 import com.google.gson.Gson;
+import com.google.sps.data.PoliceReport;
 
 @WebServlet("/analytics")
 public class AnalyticsServlet extends HttpServlet {
   private Node root;
 
   private class Node {
-    Double x, y, length;
+    Double topLeftLat, topLeftLng, bottomRightLat, bottomRightLng;
     Node NW, NE, SE, SW;
+    Boolean leaf = true;
+    int depth;
     int numReports;
-    ArrayList[] reports;
+    ArrayList<PoliceReport> reports;
+
+    Node(Double topLeftLat, Double topLeftLng, Double bottomRightLat, Double bottomRightLng,
+        ArrayList<PoliceReport> reports, int depth) {
+      this.topLeftLat = topLeftLat;
+      this.topLeftLng = topLeftLng;
+      this.bottomRightLat = bottomRightLat;
+      this.bottomRightLng = bottomRightLng;
+      this.depth = depth;
+      this.numReports = reports.size();
+      this.reports = reports;
+    }
+  }
+
+  public Node createTree() {
+    return new Node(90.0, -180.0, -90.0, 180.0, new ArrayList<PoliceReport>(), 0);
   }
 
 }
