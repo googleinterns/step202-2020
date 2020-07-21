4b1c2cc2d8410b381df3b8d61904155a8c121ed5
Ashley Wong <ashleywong@college.harvard.edu>
2020-07-21 00:36:46 +0000

    begin printing

--- web-app/src/main/java/com/google/sps/servlets/AnalyticsServlet.java
+++ web-app/src/main/java/com/google/sps/servlets/AnalyticsServlet.java
@@ -5,7 +5,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Queue;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
 import com.google.gson.Gson;
@@ -37,6 +39,16 @@ public class AnalyticsServlet extends HttpServlet {
 
   public Node createTree() {
     return new Node(90.0, -180.0, -90.0, 180.0, new ArrayList<PoliceReport>(), 0);
+  }
+
+  public void printTree(Node root) {
+    int currentLevel = root.depth;
+    Queue<Node> nodesToPrint = new ArrayDeque<Node>();
+    nodesToPrint.add(root);
+
+    while (!nodesToPrint.isEmpty()) {
+      Node node = nodesToPrint.pollFirst();
+    }
   }
 
 }
