02b0beb4c3f49f4439ec7ebef8953c90519e71e7
Jimin Byun <jiminb159@gmail.com>
2020-07-22 23:09:59 +0000

    Add template for servlet

---
+++ web-app/src/main/java/com/google/sps/servlets/AnalyticsServlet.java
@@ -0,0 +1,21 @@
+package com.google.sps.servlets;
+
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.google.gson.Gson;
+import com.google.sps.data.PoliceReport;
+import com.google.sps.data.QuadTree;
+
+@WebServlet("/analytics")
+public class AnalyticsServlet extends HttpServlet {
+
+  private QuadTree reportsTree;
+
+  @Override
+  public void init() {
+    Quadt
+  }
+}
