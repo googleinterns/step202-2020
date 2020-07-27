273981548e5bc99506999e96de68a8c841268eb5
Jimin Byun <jiminb159@gmail.com>
2020-07-27 05:16:52 +0000

    Add function to get n most frequent crimeTypes from reprots

--- web-app/src/main/java/com/google/sps/data/Analysis.java
+++ web-app/src/main/java/com/google/sps/data/Analysis.java
@@ -1,13 +1,14 @@
 package com.google.sps.data;
 
 import java.util.HashMap;
-import java.util.Map;
+import java.util.Map.Entry;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Collections;
 
 public class Analysis {
-  private int numReports;
-  private List<String> top3;
+  private final int numReports;
+  private final List<String> frequentTypes;
   private HashMap<String, Integer> crimeTypeCountMap;
 
   private void countCrimeType(List<PoliceReport> reports) {
@@ -24,10 +25,24 @@ public class Analysis {
     }
   }
 
-  private List<String> getMaxThreeTypes() {
+  private List<String> getTopNTypes(List<PoliceReport> reports, int n) {
+    countCrimeType(reports);
+
+    List<Entry<String, Integer>> crimeTypeFrequencyPairList = new ArrayList<>(crimeTypeCountMap.entrySet());
+    crimeTypeFrequencyPairList.sort(Entry.comparingByValue());
+    List<String>frequentTypes = new ArrayList<String>();
+
+    int count = 0;
+    while (count < n) {
+      frequentTypes.add((crimeTypeFrequencyPairList.get(crimeTypeFrequencyPairList.size()-1-count)).getKey());
+      count += 1;
+    }
+
+    return frequentTypes;
   }
 
-  public Analysis(List<PoliceReport> reports) {
+  public Analysis(List<PoliceReport> reports, int n) {
     this.numReports = reports.size();
+    this.frequentTypes = getTopNTypes(reports, n);
   }
 }
