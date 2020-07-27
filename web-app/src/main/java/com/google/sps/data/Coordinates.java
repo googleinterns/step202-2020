cb4a7f2e5ce5677984aa7435cae8e22a7eec62de
Ashley Wong <ashleywong@college.harvard.edu>
2020-07-27 04:34:53 +0000

    new coordinates object

---
+++ web-app/src/main/java/com/google/sps/data/Coordinates.java
@@ -0,0 +1,11 @@
+package com.google.sps.data;
+
+public class Coordinates {
+  private final double lat;
+  private final double lng;
+
+  public Coordinates(double latitude, double longitude) {
+    this.lat = latitude;
+    this.lng = longitude;
+  }
+}
