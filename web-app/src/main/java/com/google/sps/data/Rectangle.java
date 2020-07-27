f004797bdc87035c81a0e23fd6aaf3e937a6797d
Ashley Wong <ashleywong@college.harvard.edu>
2020-07-27 05:16:51 +0000

    change intersect method

--- web-app/src/main/java/com/google/sps/data/Rectangle.java
+++ web-app/src/main/java/com/google/sps/data/Rectangle.java
@@ -1,53 +1,56 @@
 package com.google.sps.data;
 
 public class Rectangle {
-  private double topLeftLat;
-  private double topLeftLng;
-  private double bottomRightLat;
-  private double bottomRightLng;
-  private double centerLat;
-  private double centerLng;
+  private Coordinates topLeft;
+  private Coordinates bottomRight;
+  private Coordinates center;
 
-  public Rectangle(double topLeftLat, double topLeftLng, double bottomRightLat, double bottomRightLng) {
-    this.topLeftLat = topLeftLat;
-    this.topLeftLng = topLeftLng;
-    this.bottomRightLat = bottomRightLat;
-    this.bottomRightLng = bottomRightLng;
-    this.centerLat = (topLeftLat + bottomRightLat) / 2;
-    this.centerLng = (topLeftLng + bottomRightLng) / 2;
+  public Rectangle(Coordinates topLeft, Coordinates bottomRight) {
+    this.topLeft = topLeft;
+    this.bottomRight = bottomRight;
+    this.center = new Coordinates((topLeft.getLat() + bottomRight.getLat()) / 2,
+        (topLeft.getLng() + bottomRight.getLng()) / 2);
+  }
+
+  public Coordinates getTopLeft() {
+    return topLeft;
   }
 
   public double getTopLeftLat() {
-    return topLeftLat;
+    return topLeft.getLat();
   }
 
   public double getTopLeftLng() {
-    return topLeftLng;
+    return topLeft.getLng();
+  }
+
+  public Coordinates getBottomRight() {
+    return bottomRight;
   }
 
   public double getBottomRightLat() {
-    return bottomRightLat;
+    return bottomRight.getLat();
   }
 
   public double getBottomRightLng() {
-    return bottomRightLng;
+    return bottomRight.getLng();
   }
 
   /**
-  * Intersects: Return true if current rectangle intersects with the input, 
-  * by checking whether there is intersection in either longitude or latitude.
-  * Two rectangles just sharing a single edge or point is not considered as
-  * intersection. 
-  */
+   * Intersects: Return true if current rectangle intersects with the input, by
+   * checking whether there is intersection in either longitude or latitude. Two
+   * rectangles just sharing a single edge or point is not considered as
+   * intersection.
+   */
   public boolean intersects(Rectangle rect) {
     // No overlap in latitude
-    if (topLeftLat <= rect.bottomRightLat || rect.topLeftLat <= bottomRightLat) {
+    if (topLeft.getLat() <= rect.getBottomRightLat() || rect.getTopLeftLat() <= bottomRight.getLat()) {
       return false;
     }
-  
-    // No overlap in longitue  
-    if (topLeftLng >= rect.bottomRightLng || rect.topLeftLng >= bottomRightLng) { 
-      return false; 
+
+    // No overlap in longitue
+    if (topLeft.getLng() >= rect.getBottomRightLng() || rect.getTopLeftLng() >= bottomRight.getLng()) {
+      return false;
     }
 
     return true;
