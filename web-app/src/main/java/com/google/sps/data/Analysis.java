9574c508ef0d86b8cf8f359636d7fe38341c1393
Jimin Byun <jiminb159@gmail.com>
2020-07-27 00:47:51 +0000

    Add function to populate hashmap to count crimeTypes

--- web-app/src/main/java/com/google/sps/data/Analysis.java
+++ web-app/src/main/java/com/google/sps/data/Analysis.java
@@ -1,11 +1,29 @@
 package com.google.sps.data;
 
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Analysis {
   private int numReports;
   private List<String> top3;
+  private HashMap<String, Integer> crimeTypeCountMap;
+
+  private void countCrimeType(List<PoliceReport> reports) {
+    crimeTypeCountMap = new HashMap<String, Integer>();
+
+    for (PoliceReport report : reports) {
+      String crimeType = report.getCrimeType();
+      Integer count = crimeTypeCountMap.get(crimeType);
+      if (count == null) {
+        crimeTypeCountMap.put(crimeType, 1);
+      } else {
+        crimeTypeCountMap.put(crimeType, count + 1);
+      }
+    }
+  }
 
   public Analysis(List<PoliceReport> reports) {
     this.numReports = reports.size();
-    this.top3;
   }
 }
