0eb839e29058acae1794b1804c9d23f4ac6cb038
Ashley Wong <ashleywong@college.harvard.edu>
2020-07-30 00:51:44 +0000

    add new files

---
+++ test/src/main/java/org/sample/Distance.java
@@ -0,0 +1,56 @@
+package org.sample;
+
+import java.lang.Math;
+
+public class Distance {
+  private static double square(double num) {
+    return (num * num);
+  }
+
+  private static double distanceSquared(Coordinates start, Coordinates end) {
+    return square(start.getLng() - end.getLng()) + square(start.getLat() - end.getLat());
+  }
+
+  public static double distanceFromSegment(Coordinates start, Coordinates end, Coordinates point) {
+    double segmentDistanceSquared = distanceSquared(start, end);
+
+    if (segmentDistanceSquared == 0) {
+      return Math.sqrt(distanceSquared(start, point));
+    }
+
+    double projectionScale = ((point.getLng() - start.getLng()) * (end.getLng() - start.getLng())
+        + (point.getLat() - start.getLat()) * (end.getLat() - start.getLat())) / segmentDistanceSquared;
+    projectionScale = Math.max(0, Math.min(1, projectionScale));
+
+    Coordinates projection = new Coordinates(start.getLat() + projectionScale * (end.getLat() - start.getLat()),
+        start.getLng() + projectionScale * (end.getLng() - start.getLng()));
+
+    return Math.sqrt(distanceSquared(point, projection));
+  }
+
+  public static Rectangle getQueryRange(Coordinates[] waypoints) {
+    double minLongitude = 190.0;
+    double maxLongitude = -190.0;
+    double minLatitude = 100.0;
+    double maxLatitude = -100.0;
+
+    for (Coordinates waypoint : waypoints) {
+      double latitude = waypoint.getLat();
+      double longitude = waypoint.getLng();
+      if (latitude > maxLatitude) {
+        maxLatitude = latitude;
+      }
+      if (latitude < minLatitude) {
+        minLatitude = latitude;
+      }
+      if (longitude > maxLongitude) {
+        maxLongitude = longitude;
+      }
+      if (longitude < minLongitude) {
+        minLongitude = longitude;
+      }
+    }
+
+    return new Rectangle(new Coordinates(maxLatitude, minLongitude), new Coordinates(minLatitude, maxLongitude));
+  }
+}
