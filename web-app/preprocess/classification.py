4af0f001007729f7548fe5d93b06b26ee491c08c
Jimin Byun <jiminb@andrew.cmu.edu>
2020-08-11 01:34:38 +0900

    Simplify the input crimeType using nltk tools

--- web-app/preprocess/classification.py
+++ web-app/preprocess/classification.py
@@ -1,12 +1,29 @@
 from gensim import models
 import sys
+import nltk
+import string
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
 
-w = models.KeyedVectors.load_word2vec_format(
-    '../../GoogleNews-vectors-negative300.bin', binary=True)
+
+def simplifyText(crimeType):
+    crimeType = crimeType.lower()
+    crimeTypeWords = nltk.word_tokenize(crimeType)
+    crimeTypeWordsNoPunct = [word for word in crimeTypeWords if word not in string.punctuation]
+    crimeTypeWordsNoStopwords = [word for word in crimeTypeWordsNoPunct if word not in stopwords.words('english')]
+    lemmatizer = WordNetLemmatizer()
+    lemmatizedCrimeTypeWords = [lemmatizer.lemmatize(word) for word in crimeTypeWordsNoStopwords]
+    
+    return lemmatizedCrimeTypeWords
+
+#w = models.KeyedVectors.load_word2vec_format(
+#    '../../GoogleNews-vectors-negative300.bin', binary=True)
 
 categories = ['theft', 'violence', 'drugs']
 word = sys.argv[1]
 
+simplifyText(word)
+"""
 maxSimilarity = 0
 crimeType = ""
 for cat in categories:
@@ -15,4 +32,4 @@ for cat in categories:
     if (maxSimilarity < similarity):
         maxSimilarity = similarity
         crimeType = cat
-print(crimeType)
+print(crimeType)"""
