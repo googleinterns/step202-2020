4c4cde47b6606233e839e9355805ed42a3441f01
Jimin Byun <jiminb@andrew.cmu.edu>
2020-08-13 02:04:40 +0900

    Implement nearest neighbor function

--- web-app/preprocess/classification.py
+++ web-app/preprocess/classification.py
@@ -7,6 +7,7 @@ import os
 import pickle
 import numpy as np
 import fasttext.util
+from scipy.spatial.distance import cosine
 
 
 def simplifyText(crimeType):
@@ -21,43 +22,60 @@ def simplifyText(crimeType):
     
     return lemmatizedCrimeTypeWords
 
-def saveDict(categoryDict):
-    with open('categoryDict.pkl', 'wb') as f:
-        pickle.dump(categoryDict, f, pickle.HIGHEST_PROTOCOL)
+def saveDict(dictName, dictToSave):
+    with open(dictName + '.pkl', 'wb') as f:
+        pickle.dump(dictToSave, f, pickle.HIGHEST_PROTOCOL)
 
 def loadDict():
-    if (os.path.isfile('categoryDict.pkl')):
-        with open('categoryDict.pkl', 'rb') as f:
+    if (os.path.isfile('categoryClassificationDict.pkl')):
+        with open('categoryClassificationDict.pkl', 'rb') as f:
             return pickle.load(f)
     return {}
 
-def generateWordVector(lemmatizedCrimeTypeWords):
-    ft = fasttext.load_model('cc.en.300.bin')
+def loadpkl(filename):
+     with open(filename + '.pkl', 'rb') as f:
+        return pickle.load(f)
+
+def generateWordVector(lemmatizedCrimeTypeWords, ft):
     for i in range(len(lemmatizedCrimeTypeWords)):
         if i == 0:
             wordVector = np.copy(ft.get_word_vector(lemmatizedCrimeTypeWords[i]))
         else: wordVector += ft.get_word_vector(lemmatizedCrimeTypeWords[i])
     return wordVector
 
+def nearestNeighbor(crimeTypeWordVector):
+    categoryDict = loadpkl("categoryDict")
+    crimeTypeWordVectorsDict = loadpkl("crimeTypeWordVectorsDict")
+
+    nearest = None
+    maxCosSimilarity = 0
+    for crimeType in categoryDict:
+        cosSimilarity = 1 - cosine(crimeTypeWordVectorsDict[crimeType], crimeTypeWordVector)
+        if cosSimilarity > maxCosSimilarity:
+            maxCosSimilarity = cosSimilarity
+            nearest = categoryDict[crimeType]
+    return nearest
 
 def classify(crimeType):
     lemmatizedCrimeTypeWords = simplifyText(crimeType)
     lemmatizedCrimeType = ' '.join(lemmatizedCrimeTypeWords)    
 
-    categoryDict = loadDict()
-    matchingCategory = categoryDict.get(lemmatizedCrimeType, None)
+    categoryClassificationDict = loadDict()
+    matchingCategory = categoryClassificationDict.get(lemmatizedCrimeType, None)
     if matchingCategory != None:
         return matchingCategory
 
-    crimeTypeWordVector = generateWordVector(lemmatizedCrimeTypeWords)
-    #classifiedCategory = knn(crimeTypeWordVector)
+    ft = fasttext.load_model('cc.en.300.bin')
+    crimeTypeWordVector = generateWordVector(lemmatizedCrimeTypeWords, ft)
+    classifiedCategory = nearestNeighbor(crimeTypeWordVector)
+    print(classifiedCategory)
 
-    #categoryDict[lemmatizedCrimeType] = classifiedCategory
-    #saveDict(categoryDict)
+    #categoryClassificationDict[lemmatizedCrimeType] = classifiedCategory
+    #saveDict("categoryClassificationDict", categoryClassificationDict)
 
     #return classifiedCategory
 
-categories = ['theft', 'violence', 'drugs']
+#categories = ['theft', 'violence', 'drugs']
 word = sys.argv[1]
 
 classify(word)
